d86359c755e71c2233514d487aa25007a307ffb3
Manuel de la Villa <manuel.villa@dti.uhu.es>
2026-03-11 09:54:29 +0100

    feature: add workflow (#1)
    * feature: add workflow
    
    * Por error en pipeline, muevo fichero de pruebas generado por  Spring
    Initializr a com.CMEPPS.listatareas
    
    * Error previo. Specify the Configuration Class in your Test.
    
    ---------
    
    Co-authored-by: Usuario <Usuario@host.docker.internal>

---
+++ src/test/java/com/CMEPPS/listatareas/ListaTareasApplicationTests.java
@@ -0,0 +1,17 @@
+package com.CMEPPS.listatareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ListaTareasApplication.class)
+public class ListaTareasApplicationTests {
+    // Your tests...
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
